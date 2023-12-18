5b95675134969cdf1cabf09030429464177826c0
dianaoanasft <diana.oana.vrote@gmail.com>
2023-12-18 20:03:24 +0200

    Am incarcat laboratorul 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,20 @@
-﻿namespace Laborator7
+﻿using System;
+using Laborator7.Data;
+using System.IO;
+
+namespace Laborator7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database; 
+        public static ShoppingListDatabase Database { 
+            get { 
+                if (database == null) { 
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+                } 
+                return database; 
+            } 
+        }
         public App()
         {
             InitializeComponent();
---
+++ Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+
+namespace Laborator7.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
